4c04902d1f93de8c9a0b49adbcb4b146c95bf53c
SJCMN <sam.clavette@gmail.com>
2021-10-01 16:06:59 -0500

    Initial commit, set up HTML, link styles, connect js and jq.

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log('HELLO JS');
+
+$(readyNow);
+
+function readyNow(){
+
+    console.log('JQ READY');
+    
+
+}
+
+const employee = {
+    firstName: '',
+    lastName: '',
+    employeeID: '',
+    employeeTitle: '',
+    annualSalary: ''
+}
