d7aeb6ab2db4fd3f30d920904fef4ba699f56c1f
SJCMN <sam.clavette@gmail.com>
2021-10-01 17:02:24 -0500

    addEmployee function collects input data, adds object to array, clears inputs

--- app.js
+++ app.js
@@ -2,17 +2,50 @@ console.log('HELLO JS');
 
 $(readyNow);
 
+// declarations
+
+const employees = []
+
 function readyNow(){
 
     console.log('JQ READY');
-    
-
+    // click listeners
+    $( '#submitEmployee' ).on( 'click', addEmployee )
 }
 
-const employee = {
-    firstName: '',
-    lastName: '',
-    employeeID: '',
-    employeeTitle: '',
-    annualSalary: ''
+
+
+function addEmployee () {
+    
+    // employee object
+    const employee = {
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        employeeId: $('#employeeId').val(),
+        employeeTitle: $('#employeeTitle').val(),
+        annualSalary: parseInt($('#annualSalary').val())
+    }
+
+    // push input fields to array
+
+    employees.push(employee)
+    
+    console.log(employees);
+    
+    // clear fields
+
+    // for ( let el of employee) {
+    //     console.log(el);
+    //     el.val('')
+    // }
+
+
+
+    $('#firstName').val(''),
+    $('#lastName').val(''),
+    $('#employeeId').val(''),
+    $('#employeeTitle').val(''),
+    $('#annualSalary').val('')
+
+
 }
