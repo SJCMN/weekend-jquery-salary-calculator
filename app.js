788df2072ab46157bdc165aa378d4fe49c29c663
SJCMN <sam.clavette@gmail.com>
2021-10-03 22:37:44 -0500

    Did some refactoring...consolodate variables deletedSalary, salaryMonthly. Use addSalaries() to append salary calc to dom

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ $(readyNow);
 
 let salaryTotal = 0
 let salaryMonthly = 0
+let deletedSalary = 0
 const cap = 40000
 
 const employees = [
@@ -92,12 +93,27 @@ function addEmployee () {
 
 }
 
+function removeEmployee() {
+    // match deleted salary to annualSalary
+    for ( let i = 0; i < employees.length; i ++) {
+        console.log('checking this employee: ', employees[i]);
+        console.log('the employee salary is: ', employees[i].annualSalary);
+        if ( employees[i].annualSalary === parseInt(deletedSalary) ) {
+        employees.splice(i,1)
+        console.log('the removed employee is: ', employees[i]);
+        
+        }
+        addSalaries()
+    }
+}
+
 
 // render array of objects to DOM
 
 function renderEmployee () {
     // clear table elements on DOM
     $('.employeeRow').empty();
+
 
     for (let employee of employees) {
 
@@ -123,17 +139,19 @@ function renderEmployee () {
 
 
 function addSalaries(){
-    // total salaries for employees  
+    // total salaries for employees 
+    salaryTotal = 0; 
     for (let employee of employees){
         salaryTotal += employee.annualSalary
-    }
+    };
 
     // calc monthly salary budget
-    salaryMonthly = salaryTotal/12
+    salaryMonthly = salaryTotal/12;
 
-    salaryCap ()
+    salaryCap ();
 
 }
+
 
 function salaryCap () {
         // check if monthly salary exceeds cap
@@ -158,30 +176,31 @@ function deleteSalary() {
 
     // console.log(this);
     let deletedRow = $( this ).parent().parent().text();
-    let deletedSalary = deletedRow.replace(/[^\d.-]/g, '')
+    deletedSalary = deletedRow.replace(/[^\d.-]/g, '')
 
     // removes first 4 numbers of remaining number string
     // removes employee id from salary string
 
     // subtract deleted salary from salaryTotal
     console.log('this is the deleted salary: ', deletedSalary.slice(4));
-    console.log('this is the new total salary: ',salaryTotal-deletedSalary.slice(4));
+    deletedSalary = deletedSalary.slice(4)
+    console.log('this is the new total salary: ', salaryTotal-deletedSalary.slice(4));
     salaryTotal -= (deletedSalary.slice(4));
-    console.log('this is the new salaryTotal value: ',salaryTotal);
-
-    deletedSalary = salaryTotal/12
-    console.log('this is the new salaryTotal/12: ',deletedSalary);  
+    console.log('this is the new salaryTotal value: ', salaryTotal);
+    salaryMonthly = salaryTotal/12
+    console.log('this is the new salaryTotal/12: ', salaryMonthly);  
     salaryCap ()
-    appendRemaining(deletedSalary)
+    // appendRemaining()
+    removeEmployee()
 }
 
 
-function appendRemaining(salary){
+// function appendRemaining(){
     
-    // append dom with formatted salary number
-    $( '#salaryTotal' ).text(formatCurrency(salary))
+//     // append dom with formatted salary number
+//     $( '#salaryTotal' ).text(formatCurrency(salaryMonthly))
     
-}
+// }
 
 
 // returns currency formatted number
